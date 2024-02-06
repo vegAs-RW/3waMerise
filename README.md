bd7ed5b930b725525e6e75a197a9d3c0b07c552e
vegAs-RW <jordan.pouchot2@gmail.com>
2024-02-06 12:27:41 +0100

    evaluation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 3waMerise
+
+Cas pratique : Mise en place d'un LMS (Learning Management System)
+Expression des besoins du client : 
+
+Le client souhaite faire développer un site/application permettant de gérer toutes les ressources pédagogiques. Les étudiants et formateurs pourront s'inscrire dessus et s'y connecter grâce à leurs identifiants (email, mot de passe). 
+
+Un étudiant fait partie d'une seule session à la fois. Les formateurs peuvent donner cours à plusieurs sessions. Chaque session sera composée de différents cours qui auront un nom (par exemple Symfony) et une date (par exemple du 01/01 au 05/01) de manière à pouvoir créer un emploi du temps, que l'étudiant pourra consulter à chaque instant. Une session doit avoir un nom unique pour qu'on puisse la retrouver facilement. La plateforme permettra aux étudiants d'afficher tout le contenu des cours. Pour chaque session on pourra afficher la liste des étudiants qui la composent ainsi qu'un "trombinoscope" (toutes les photos de tous les étudiants de la session).
+
+Les formateurs peuvent créer des évaluations directement sur la plateforme. Ces évaluations pourront être de différents types (qcm, tp...). Les formateurs peuvent donner des notes aux étudiants sur ces évaluations.
+
+Travail à réaliser
+Réaliser un MCD (une image) qui représentera l'ensemble des données de ce projet, un MLD (un fichier texte) avec la liste de toutes les tables et leurs relations, un script SQL pour mettre en place cette base de données.
+
+Le travail sera déposé sur un dépôt github public. L'url du dépôt peut être déposé à cette url.
---
+++ sql.sql
@@ -0,0 +1,73 @@
+CREATE TABLE users(
+   user_id INT AUTO_INCREMENT,
+   mail VARCHAR(50),
+   password VARCHAR(150),
+   firstname VARCHAR(50),
+   lastname VARCHAR(50),
+   PRIMARY KEY(user_id)
+);
+
+CREATE TABLE session(
+   session_id INT AUTO_INCREMENT,
+   name VARCHAR(50),
+   PRIMARY KEY(session_id),
+   UNIQUE(name)
+);
+
+CREATE TABLE student(
+   user_id INT,
+   profilPicture BLOB,
+   session_id INT,
+   PRIMARY KEY(user_id),
+   FOREIGN KEY(user_id) REFERENCES users(user_id),
+   FOREIGN KEY(session_id) REFERENCES session(session_id)
+);
+
+CREATE TABLE teacher(
+   user_id INT AUTO_INCREMENT,
+   PRIMARY KEY(user_id),
+   FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE course(
+   course_id INT AUTO_INCREMENT,
+   name VARCHAR(50),
+   start_date DATETIME,
+   end_date DATETIME,
+   content TEXT,
+   user_id INT,
+   PRIMARY KEY(course_id),
+   FOREIGN KEY(user_id) REFERENCES teacher(user_id)
+);
+
+CREATE TABLE evaluation(
+   evaluation_id INT AUTO_INCREMENT,
+   course_id INT,
+   user_id INT NOT NULL,
+   PRIMARY KEY(evaluation_id),
+   FOREIGN KEY(course_id) REFERENCES course(course_id),
+   FOREIGN KEY(user_id) REFERENCES teacher(user_id)
+);
+
+CREATE TABLE contain(
+   session_id INT,
+   course_id INT,
+   PRIMARY KEY(session_id, course_id),
+   FOREIGN KEY(session_id) REFERENCES session(session_id),
+   FOREIGN KEY(course_id) REFERENCES course(course_id)
+);
+
+CREATE TABLE note(
+   note_id INT,
+   evaluation_id INT,
+   PRIMARY KEY(note_id),
+   FOREIGN KEY(evaluation_id) REFERENCES evaluation(evaluation_id)
+);
+
+CREATE TABLE has(
+   evaluation_id INT,
+   category_id INT,
+   PRIMARY KEY(category_id, evaluation_id),
+   FOREIGN KEY(evaluation_id) REFERENCES evaluation(evaluation_id)
+   FOREIGN KEY(category_id) REFERENCES category(category_id)
+);
